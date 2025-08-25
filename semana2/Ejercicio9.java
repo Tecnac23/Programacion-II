714583a0167c4a071ef9148023594b3cf2c57a99
Ignacio <carne_ia@hotmail.com>
2025-08-25 16:35:47 -0300

    Agregué ajercicio Nº 9 al TP2. Ahora quedó completo

--- semana2/Ejercicio9.java
+++ semana2/Ejercicio9.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package tp2;
+package EjerciciosTP2;
 
 import java.util.Scanner;
 
@@ -38,7 +38,7 @@ public class Ejercicio12{
         
         totalAPagar = precioProducto + costoEnvio;
         
-        System.out.println("El total a pagar es: " + totalAPagar + " pesos.");
+        System.out.println("El total a pagar es " + totalAPagar + " pesos.");
                
     }
     
